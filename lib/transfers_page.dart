382067555b7dc6b9b589ec7ec6ec0eab2ac41206
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-08-07 12:33:30 -0300

    Tranfers Page

--- lib/transfers_page.dart
+++ lib/transfers_page.dart
@@ -1,14 +1,25 @@
+import 'package:fitbank_alura/pages/trasfers_tile.dart';
 import 'package:flutter/material.dart';
 
 class TransfersPage extends StatelessWidget {
+  const TransfersPage({
+    super.key,
+  });
 
-  const TransfersPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      body: const Column(
+        children: [
+          TrasfersTile(account: 'Conta 0001-1111-0000', value: 100),
+          TrasfersTile(account: 'Conta 0001-1111-0000', value: 200),
+          TrasfersTile(account: 'Conta 0001-1111-0000', value: 300),
+        ],
+      ),
+    );
   }
 }
